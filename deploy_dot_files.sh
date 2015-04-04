0cea3f7594fee1fe740467059678da4176a007ff
Adam Holsinger <adamdude828@gmail.com>
2015-04-04 12:02:37 -0600

    progress

--- deploy_dot_files.sh
+++ deploy_dot_files.sh
@@ -16,19 +16,19 @@ groupadd dotfiles
 
 linkResource() {
     getCurrentLocation
-    runFromHere $1 "sudo -u $1 rm -rf $2"
-    runFromHere $1 "sudo -u $1 ln -s ${current_location}files/dot/$2 $2"
+    runFromHere $1 "sudo -u $2 rm -rf $3"
+    runFromHere $1 "sudo -u $2 ln -s ${current_location}files/dot/$3 $3"
 #    usermod -aG dotfiles $1
 }
 
 for user in $(find /home/ -maxdepth 1 -mindepth 1 -type d -printf '%f\n')
 do
-    linkResource "/home/$user/" ".vim"
-    linkResource "/home/$user/" ".bashrc"
-    linkResource "/home/$user/" ".vimrc"
+    linkResource "/home/$user/" $user ".vim"
+    linkResource "/home/$user/" $user  ".bashrc"
+    linkResource "/home/$user/" $user ".vimrc"
 done
 
-linkResource "/root" ".vim"
-linkResource "/root" ".bashrc"
-linkResource "/root" ".vimrc"
+linkResource "/root" "root" ".vim"
+linkResource "/root" "root" ".bashrc"
+linkResource "/root" "root" ".vimrc"
 
